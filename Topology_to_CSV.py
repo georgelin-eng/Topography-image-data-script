e2a79bff268c2af2e47f022b34d6fd9cd6f62a5d
George <flipatable2018@gmail.com>
2023-03-25 23:37:56 -0700

    Fixed formatting and change RGB -> pgh processing

--- Topology_to_CSV.py
+++ Topology_to_CSV.py
@@ -3,7 +3,7 @@ from PIL import Image
 im = Image.open ('topology.png')
 width, height = im.size
 
-# file output
+# file output settings
 import csv
 file = open('Topology.csv', 'w',newline='')
 writer = csv.writer(file)
@@ -11,26 +11,23 @@ header = ['X', 'Y', 'pgh']
 writer.writerow(header)
 
 # iterate through topologyData to find the pgh value that corresponds to the R value of the x-y position
-def match(R, topologyData):
+def match(R,G,B , topologyData):
+    deviation = 0
+    threshhold = 10 #allowable amount of deviation from exact RGB pixel value
     for i in range (13):
-        if R == topologyData[i][0]:
+        deviation = abs(topologyData[i][0] - R) + abs(topologyData[i][1] - G) + abs(topologyData[i][2] - B)
+        if deviation < threshhold:
             return topologyData [i][3]
 
 # initialization of arrays
 x_y_pgh = [0, 0 , 0]
 topologyData = [[209, 250, 254, -2.5], #format [R,G,B, pgh]
-                [194, 241, 195, 0],
-                [176, 230, 119, 2.5],
-                [145, 204, 110, 5.0],
-                [112, 180, 103, 7.5],
-                [79, 158, 95, 10.0], 
-                [55, 139, 89, 12.5],
-                [49, 121, 73, 15.0],
-                [64, 107, 53, 17.5],
-                [77, 95, 37, 20.0],
-                [90, 96, 36, 22.5],
-                [103, 98, 42, 25.0],
-                [115, 99, 47, 27.5],
+                [194, 241, 195, 0], [176, 230, 119, 2.5],
+                [145, 204, 110, 5.0],[112, 180, 103, 7.5],
+                [79, 158, 95, 10.0], [55, 139, 89, 12.5],
+                [49, 121, 73, 15.0], [64, 107, 53, 17.5],
+                [77, 95, 37, 20.0], [90, 96, 36, 22.5],
+                [103, 98, 42, 25.0], [115, 99, 47, 27.5],
                 [137, 101, 57, 30.0]] 
 
 for x in range (0, width, 2):
@@ -42,7 +39,7 @@ for x in range (0, width, 2):
 
             x_y_pgh [0]= x_pos
             x_y_pgh [1]= y_pos
-            x_y_pgh [2] = match (R, topologyData) 
+            x_y_pgh [2] = match (R,G,B, topologyData) 
             writer.writerow (x_y_pgh)
 
 file.close()
