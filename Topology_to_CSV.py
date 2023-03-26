7f658fb9b2e098fa1ee7c851ba40ddd9a3667d8e
George <flipatable2018@gmail.com>
2023-03-25 23:24:52 -0700

    Topology data fixed

--- Topology_to_CSV.py
+++ Topology_to_CSV.py
@@ -2,7 +2,6 @@
 from PIL import Image
 im = Image.open ('topology.png')
 width, height = im.size
-print(width, height)
 
 # file output
 import csv
@@ -13,7 +12,7 @@ writer.writerow(header)
 
 # iterate through topologyData to find the pgh value that corresponds to the R value of the x-y position
 def match(R, topologyData):
-    for i in range (9):
+    for i in range (13):
         if R == topologyData[i][0]:
             return topologyData [i][3]
 
@@ -28,7 +27,11 @@ topologyData = [[209, 250, 254, -2.5], #format [R,G,B, pgh]
                 [55, 139, 89, 12.5],
                 [49, 121, 73, 15.0],
                 [64, 107, 53, 17.5],
-                [77, 95, 37, 20.0]] 
+                [77, 95, 37, 20.0],
+                [90, 96, 36, 22.5],
+                [103, 98, 42, 25.0],
+                [115, 99, 47, 27.5],
+                [137, 101, 57, 30.0]] 
 
 for x in range (0, width, 2):
     for y in range (0, height, 2):
