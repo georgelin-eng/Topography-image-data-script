4e5e172ef5090952005e988d4a50815059ea2ca5
George <flipatable2018@gmail.com>
2023-03-25 17:32:06 -0700

    Starting on topology image to CSV pgh data

---
+++ # Constants.py
@@ -0,0 +1,16 @@
+# Constants that are used throughout the project
+
+#Constants
+gravity = 9.81
+water_density = 1000
+Pipe_diam = 0.02
+Pipe_frict = 0.05
+K_loss = 10
+Rcost = 100000
+
+#Satisfaction constants
+
+Consumption_min = 115
+Consumption_max = 675
+Cost_min = Rcost*0.25
+Cost_max = Rcost*1.30
