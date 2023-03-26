a84b5a18a9fcd421f079085d66d0fecdbc477827
George <flipatable2018@gmail.com>
2023-03-25 21:31:41 -0700

    CSV output added

--- Topology_to_CSV.py
+++ Topology_to_CSV.py
@@ -3,15 +3,21 @@ from PIL import Image
 im = Image.open ('topology.png')
 width, height = im.size
 
+# file output
+import csv
+file = open('Topology.csv', 'w',newline='')
+writer = csv.writer(file)
+header = ['X', 'Y', 'pgh']
+writer.writerow(header)
+
 # iterate through topologyData to find the pgh value that corresponds to the R value of the x-y position
 def match(R, topologyData):
     for i in range (9):
         if R == topologyData[i][0]:
             return topologyData [i][3]
 
-
 # initialization of arrays
-x_y_pgh = [[0] *3 for i in range (width)]
+x_y_pgh = [0, 0 , 0]
 topologyData = [[209, 250, 254, -2.5], #format [R,G,B, pgh]
                 [194, 241, 195, 0],
                 [176, 230, 119, 2.5],
@@ -30,8 +36,11 @@ for x in range (width):
         y_pos  = -float(y)/2 + 100
         x_pos = float(x)/2.3 - 20
 
-        x_y_pgh [x][0] = round(x_pos, 0)
-        x_y_pgh [x][1] = y_pos
-        pgh = match (R, topologyData) 
-        x_y_pgh [x][2] = pgh
+        x_y_pgh [0]= round(x_pos, 0)
+        x_y_pgh [1]= y_pos
+        x_y_pgh [2] = match (R, topologyData) 
+        writer.writerow (x_y_pgh)
+
+file.close()
+
 
